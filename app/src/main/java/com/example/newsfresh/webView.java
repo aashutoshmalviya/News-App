73bc35b01a61b5aaf09aa089da3da18c0e16f1c7
Ashutosh Malviya <aashutoshmalviya2@gmail.com>
2022-10-09 19:56:02 +0530

    Gui working fine

---
+++ app/src/main/java/com/example/newsfresh/webView.java
@@ -0,0 +1,14 @@
+package com.example.newsfresh;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class webView extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+    }
+}
